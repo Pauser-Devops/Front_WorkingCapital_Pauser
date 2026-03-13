1414581a173524f01c5383cac85b4eaefddded43
edwardqs <ventas1@pauserdistribucionessac.onmicrosoft.com>
2026-03-12 23:03:52 -0500

    cambios en http a https, ngix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://desarrolllo-backend-wk-pauser-m7w7tf-83cf2e-161-132-48-71.traefik.me'
+  apiUrl: 'https://desarrolllo-backend-wk-pauser-m7w7tf-83cf2e-161-132-48-71.traefik.me'
 };
